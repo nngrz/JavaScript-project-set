d5628acb1ebdbe3a6df5cc0cd8fbd14c09372d2c
Ningrui Zhang <ningrui1227@gmail.com>
2024-07-26 14:29:33 +0100

    update chrome_extension

---
+++ test/test.js
@@ -0,0 +1,13 @@
+// template strings/literals
+
+const recipient = "James"
+const sender = "Per Harald Borgen"
+
+// Break the email string into multiple lines
+const email = `Hey ${recipient}! 
+
+How is it going? 
+
+Cheers ${sender}`
+
+console.log(email)
